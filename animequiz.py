0e4f3ad6cad57a9c1bc8ebb2c1190e00759ca37c
Kejax <kejax@kejax.net>
2023-05-05 23:14:36 +0200

    Changed efficiency

--- animequiz.py
+++ animequiz.py
@@ -1,93 +1,24 @@
 score = 0
 
-# Question 1
-print("Question 1: What is the name of the main character in Naruto?")
-answer = input("Your answer: ")
-if answer.lower() == "naruto":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect. The correct answer is Naruto.")
-
-# Question 2
-print("Question 2: In which anime does a character named Monkey D. Luffy appear?")
-answer = input("Your answer: ")
-if answer.lower() == "one piece":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect. The correct answer is One Piece.")
-
-# Question 3
-print("Question 3: What is the name of the protagonist in Attack on Titan?")
-answer = input("Your answer: ")
-if answer.lower() == "eren yeager" or answer.lower() == "eren jaeger":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect. The correct answer is Eren Yeager/Jaeger.")
-
-# Question 4
-print("Question 4: In which anime do characters called Titans appear?")
-answer = input("Your answer: ")
-if answer.lower() == "attack on titan":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect. The correct answer is Attack on Titan.")
-
-# Question 5
-print("Question 5: What is the name of the main character in Death Note?")
-answer = input("Your answer: ")
-if answer.lower() == "light yagami":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect. The correct answer is Light Yagami.")
-
-# Question 6
-print("Question 6: Who is the main character in Fullmetal Alchemist?")
-answer = input("Your answer: ")
-if answer.lower() == "edward elric" or answer.lower() == "edward":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect. The correct answer is Edward Elric.")
-
-# Question 7
-print("Question 7: In which anime does a character named Natsu Dragneel appear?")
-answer = input("Your answer: ")
-if answer.lower() == "fairy tail":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect. The correct answer is Fairy Tail.")
-
-# Question 8
-print("Question 8: What is the name of the main character in Sword Art Online?")
-answer = input("Your answer: ")
-if answer.lower() == "kirito" or answer.lower() == "kazuto kirigaya":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect. The correct answer is Kirito/Kazuto Kirigaya.")
-
-# Question 9
-print("Question 9: In which anime does a character named Kagome Higurashi appear?")
-answer = input("Your answer: ")
-if answer.lower() == "inuyasha":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect. The correct answer is Inuyasha.")
-
-# Question 10
-print("Question 10: What is the name of the main character in One Punch Man?")
-answer = input("Your answer: ")
-if answer.lower() == "saitama":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect. The correct answer is Saitama.")
+questions = {
+    "Question 1: What is the name of the main character in Naruto?": "Naruto",
+    "Question 2: In which anime does a character named Monkey D. Luffy appear?": "One Piece",
+    "Question 3: What is the name of the protagonist in Attack on Titan?": ["Eren Yeager", "Eren Jaeger"],
+    "Question 4: In which anime do characters called Titans appear?": "Attack on Titan",
+    "Question 5: What is the name of the main character in Death Note?": "Light Yagami",
+    "Question 6: Who is the main character in Fullmetal Alchemist?": ["Edward", "Edward Elric", "Elric"],
+    "Question 7: In which anime does a character named Natsu Dragneel appear?": "Fairy Tail",
+    "Question 8: What is the name of the main character in Sword Art Online?": ["Kirito", "Kazuto Kirigaya"],
+    "Question 9: In which anime does a character named Kagome Higurashi appear?": "Inuyasha",
+    "Question 10: What is the name of the main character in One Punch Man?": "Saitama"
+}
+    
+for key, value in questions.items():
+    answer = input(key + "\n")
+    if(isinstance(value, list) and answer.lower() in [i.lower() for i in value]) or (isinstance(value, str) and answer.lower() == value.lower()):
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect. The correct answer is " + ("/".join(i for i in value) if isinstance(value, list) else value))
 
 print("Your final score is", score, "out of 10.")
